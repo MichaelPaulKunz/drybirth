159931d5747b921251d47a6d5c8cd8698298a82b
michaelpaulkunz <michaelpaulkunz@gmail.com>
2023-03-14 19:40:24 -0500

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ".././style.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="center">
+        <h1 className="center">
+          DRY BIRTH
+        </h1>
+
+      </div>
+    )
+  }
+}
+
+export default App;
